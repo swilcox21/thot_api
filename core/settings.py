839a59c103c9c568aba749f72540cdbe5672cb45
Sam Wilcox <swilcox21@gmail.com>
2022-12-09 15:08:43 -0500

    makemigrations

--- core/settings.py
+++ core/settings.py
@@ -87,6 +87,13 @@ WSGI_APPLICATION = 'core.wsgi.application'
 
 # DATABASES = {
 #     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
+# DATABASES = {
+#     'default': {
 #         'ENGINE': 'django.db.backends.postgresql',
 #         'NAME': os.environ['DATABASE_URL'],
 #     }
